aacea2a17181fa35fda0d87639c118121a22273a
Miguel <moogwol@msn.com>
2026-02-16 14:32:08 +0100

    changes

--- mg-frontend/app/api.ts
+++ mg-frontend/app/api.ts
@@ -67,12 +67,19 @@ class ApiError extends Error {
 async function apiRequest<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
   const url = `${API_BASE_URL}${endpoint}`;
 
+  // const config: RequestInit = {
+  //   credentials: 'include',
+  //   ...options,
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     ...(options.headers || {})
+  //   }
+  // };
+
   const config: RequestInit = {
-    credentials: 'include',
     ...options,
     headers: {
-      'Content-Type': 'application/json',
-      ...(options.headers || {})
+      ...(options.headers || {}), 'Content-Type': 'application/json',
     }
   };
 
